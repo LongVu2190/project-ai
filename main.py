3b3a9587d900de3ea500cf0a193f267dc91463ed
LongVu2190 <hoanglongvu912@gmail.com>
2023-11-14 17:38:50 +0700

    update shadow for main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QFont, QImage, QPalette, QBrush
-from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QFont, QColor
 
 from PyQt5 import QtGui
 from dev.board import *
@@ -19,32 +18,52 @@ class DepthDialog(QDialog):
         self.setWindowTitle('Chess AI')
 
         self.depth_input = QSpinBox()
-        self.depth_input.setRange(1, 5)
+        self.depth_input.setRange(1, 6)
         self.depth_input.setValue(3)
-        self.depth_input.setFont(QFont("Arial", 16))
-        self.depth_input.setFixedSize(120, 40)
+        self.depth_input.setFont(QFont("Consolas", 16))
+        self.depth_input.setFixedSize(200, 40)
         
-
         # Apply the QGraphicsOpacityEffect to the depth_input widget
-        # Tạo một checkbox mới với nhãn là "Impossible"
-        self.checkbox = QCheckBox("Can not be defeated mode")
-        self.checkbox.setFont(QFont("Arial", 12))
-        self.checkbox.setStyleSheet("color: rgb(255,69,0); font-weight: bold; font-size: 18px")
+        self.checkbox = QCheckBox("Stockfish Mode (Depth: 15)")
+        self.checkbox.setFont(QFont("Consolas", 20))
+        self.checkbox.setStyleSheet("color: rgb(255,250,250); font-weight: bold; font-size: 20px")
         
-
         self.label = QLabel("AI Player:")
-        self.label.setFont(QFont("Arial", 14))
-        self.label.setStyleSheet("color: rgb(255,105,180); font-weight: bold; font-size: 26px")
-        self.label1 = QLabel("Depth of AI:")
-        self.label1.setStyleSheet("color: rgb(255,105,180); font-weight: bold; font-size: 26px")
-        self.label1.setFont(QFont("Arial", 14))
+        self.label.setFont(QFont("Consolas", 14))
+        self.label.setStyleSheet("color: rgb(255,250,250); font-weight: bold; font-size: 26px")
+        self.label_depth = QLabel("AI Depth:")
+        self.label_depth.setStyleSheet("color: rgb(255,250,250); font-weight: bold; font-size: 26px")
+        self.label_depth.setFont(QFont("Consolas", 14))
 
         # Tạo một dropdown box mới với hai giá trị là "black" và "white"
         self.dropdown = QComboBox()
         self.dropdown.addItems(["BLACK", "WHITE"])
-        self.dropdown.setFont(QFont("Arial", 14))
-        # self.dropdown.setStyleSheet("color: black , background: #000")
-        self.dropdown.setFixedSize(120, 40)
+        self.dropdown.setFont(QFont("Consolas", 14))
+        self.dropdown.setFixedSize(200, 40)
+
+        # Tạo hiệu ứng shadow
+        shadow1 = QGraphicsDropShadowEffect()
+        shadow1.setBlurRadius(20)
+        shadow1.setXOffset(1)
+        shadow1.setYOffset(1)
+        shadow1.setColor(QColor('black'))
+
+        shadow2 = QGraphicsDropShadowEffect()
+        shadow2.setBlurRadius(20)
+        shadow2.setXOffset(1)
+        shadow2.setYOffset(1)
+        shadow2.setColor(QColor('black'))
+
+        shadow3 = QGraphicsDropShadowEffect()
+        shadow3.setBlurRadius(20)
+        shadow3.setXOffset(1)
+        shadow3.setYOffset(1)
+        shadow3.setColor(QColor('black'))
+
+        # Thêm hiệu ứng shadow vào label
+        self.label.setGraphicsEffect(shadow1)
+        self.label_depth.setGraphicsEffect(shadow2)
+        self.checkbox.setGraphicsEffect(shadow3)
 
         self.button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         self.button_box.setStyleSheet(
@@ -55,10 +74,9 @@ class DepthDialog(QDialog):
         self.button_box.rejected.connect(self.reject)
 
         layout = QVBoxLayout()
-        #
         spacer = QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
         layout.addItem(spacer)
- #
+
         bg_layout = QVBoxLayout()
         self.label_image = QLabel(self)
         self.label_image.move(-28, 0) 
@@ -69,19 +87,28 @@ class DepthDialog(QDialog):
         self.label_image.setGraphicsEffect(opacity_effect)
         bg_layout.addWidget(self.label_image)
         layout.addChildLayout(bg_layout)
-        layout.addWidget(self.label1)
-        layout.addWidget(self.depth_input)
         layout.addWidget(self.checkbox)
-        layout.addWidget(self.label)
-        layout.addWidget(self.dropdown)
+
+        hbox1 = QHBoxLayout()
+        hbox2 = QHBoxLayout()
+
+        # Thêm các widget vào hbox
+        hbox1.addWidget(self.label_depth)
+        hbox1.addWidget(self.depth_input)
+
+        # Thêm các widget vào hbox
+        hbox2.addWidget(self.label)
+        hbox2.addWidget(self.dropdown)
+        
+        # Thêm hbox vào layout chính
+        layout.addLayout(hbox1)
+        layout.addLayout(hbox2)
+
         layout.addWidget(self.button_box)
         
         self.setLayout(layout)
         # Đặt kích thước cố định cho cửa sổ
         self.setFixedSize(450, 450)
-
-
-
 
     def getDepth(self):
         return self.depth_input.value()
