5c060b2c0d2ed6405bd32360d8c651c286980cba
LongVu2190 <hoanglongvu912@gmail.com>
2023-11-17 23:28:13 +0700

    reference

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ SCREEN_WIDTH=900
 SCREEN_TITLE="Chess AI"
 
 BOARD_SIZE=650
-# Build exe file
-# pyinstaller.exe --add-binary ".\stockfish\stockfish-windows-x86-64-avx2.exe;." --onefile .\main.py
+
+# reference: https://github.com/healeycodes/andoma
+# Elo: ~1550 Depth 4
+# build: pyinstaller.exe --add-binary ".\stockfish\stockfish-windows-x86-64-avx2.exe;." --onefile .\main.py
